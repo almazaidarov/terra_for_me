25cdd2825ede170b541a1bee551e764767ae0564
almazaidarov <almazaidarov@gmail.com>
2024-03-26 15:25:26 +0300

    addded tags

--- class1/main.tf
+++ class1/main.tf
@@ -1,3 +1,8 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name = "bitch_iam_an_engineer"
+    managed_by = "https://github.com/almazaidarov/terra_for_me"
+  }
 }
