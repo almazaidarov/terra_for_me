492d8376a43e7c2e054b45297f521440d0066f5c
almazaidarov <almazaidarov@gmail.com>
2024-03-26 15:55:20 +0300

    fixed key_pair

--- class1/main.tf
+++ class1/main.tf
@@ -31,6 +31,7 @@ resource "aws_iam_group_membership" "team" {
   group = aws_iam_group.engineers.name
 }
 
-resource "aws_key_pair" "terraform_class" {
+resource "aws_key_pair" "terraform_class"  {
   key_name   = "terraform_class"
   public_key = file("~/.ssh/id_rsa.pub")
+}
