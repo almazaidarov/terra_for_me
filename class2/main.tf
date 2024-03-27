ee76352e89739606efa94e8912dd3e6bbc590c98
almazaidarov <almazaidarov@gmail.com>
2024-03-27 09:29:26 +0300

    addded key

--- class2/main.tf
+++ class2/main.tf
@@ -21,13 +21,17 @@ resource "aws_security_group" "wordpress-terraform"{
 
 }
 
-
+resource "aws_key_pair" "terraform_class" {
+  key_name   = "terraform_class"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
 
 resource "aws_instance" "web" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t3.micro"
   associate_public_ip_address = true
   availability_zone           = "us-east-1a"
+  key_name                    = aws_key_pair.terraform_class.key_name
   user_data                   = file("wordpress.sh")
   vpc_security_group_ids      = [aws_security_group.wordpress-terraform.id]
 
