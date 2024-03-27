c1b5f0b0ac7ee6a20517ee1451f899d0448a02b8
almazaidarov <almazaidarov@gmail.com>
2024-03-27 09:20:47 +0300

    added public ip

--- class2/output.tf
+++ class2/output.tf
@@ -2,7 +2,13 @@ output id {
     description = "Provides information about instance ID"
     value = aws_instance.web.id
 }
+
 output arn {
     description = "Provides information about instance ARN"
     value = aws_instance.web.arn
+}
+
+output public_ip {
+    description = "Provides information about instance public_ip"
+    value = aws_instance.web.public_ip
 }
