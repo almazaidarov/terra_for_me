c98ee9b69bc553c3850fadef45f034a66d6621f5
almazaidarov <almazaidarov@gmail.com>
2024-03-27 11:24:12 +0300

    added alb

--- class2/main.tf
+++ class2/main.tf
@@ -81,3 +81,27 @@ module "asg" {
   ebs_optimized     = false
   enable_monitoring = false
 }
+
+data "aws_availability_zones" "all" {}
+resource "aws_elb" "bar" {
+  name               = "foobar-terraform-elbs"
+  availability_zones = data.aws_availability_zones.all.names
+  security_groups    = [aws_security_group.wordpress-terraform.id]
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "TCP:80"
+    interval            = 30
+  }
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+}
